7e0fa19f8641c87bda3f171cda213fa238576349
tristantao <tao.tristan@gmail.com>
2016-11-15 15:14:31 -0800

    init that loads from the subdirectory

--- py_ms_cognitive/__init__.py
+++ py_ms_cognitive/__init__.py
@@ -1,1 +1,2 @@
-from .py_ms_cognitive import PyMsCognitiveWebSearch
+from py_ms_cognitive_search.py_ms_cognitive_web_search import PyMsCognitiveWebSearch
+from py_ms_cognitive_search.py_ms_cognitive_news_search import PyMsCognitiveNewsSearch
