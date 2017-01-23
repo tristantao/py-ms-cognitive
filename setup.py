c941903f52147ce916795169961d8b6b94fea451
tristantao <tao.tristan@gmail.com>
2017-01-23 14:35:04 -0800

    version bump for custom params and other fixes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 import platform
 
 DESCRIPTION = "A simple lightweight python wrapper for the Microsoft Cognitive Services."
-VERSION = '0.3.1'
+VERSION = '0.3.2'
 LONG_DESCRIPTION = None
 try:
     LONG_DESCRIPTION = open('README.md').read()
