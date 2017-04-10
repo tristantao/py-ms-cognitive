11d2df819424ef251e2796a325ef77ac1e281b92
tristantao <tao.tristan@gmail.com>
2017-04-09 18:28:48 -0700

    updating the suggestions class to use new custom_params

--- py_ms_cognitive/py_ms_cognitive_search/py_ms_cognitive_suggestions.py
+++ py_ms_cognitive/py_ms_cognitive_search/py_ms_cognitive_suggestions.py
@@ -10,13 +10,12 @@ from .py_ms_cognitive_search import QueryChecker
 
 class PyMsCognitiveSuggestions(PyMsCognitiveSearch):
 
-    SEARCH_WEB_BASE = 'https://api.cognitive.microsoft.com/bing/v5.0/suggestions'
+    COGNITIVE_SUGGESTIONS_BASE = 'https://api.cognitive.microsoft.com/bing/v5.0/suggestions'
 
-    def __init__(self, api_key, query, silent_fail=False, custom_params=''):
-        query_url = self.SEARCH_WEB_BASE
-        self.custom_params_hash = dict(item.split("=") for item in custom_params.split("&")[1:])
-        PyMsCognitiveSearch.__init__(self, api_key, query, query_url, silent_fail=silent_fail)
-    
+    def __init__(self, api_key, query, custom_params={}, silent_fail=False):
+        query_url = self.COGNITIVE_SUGGESTIONS_BASE
+        PyMsCognitiveSearch.__init__(self, api_key, query, query_url, custom_params, silent_fail=silent_fail)
+
     def _search(self, limit, format):
         '''
         Returns a list of results objects
@@ -44,7 +43,7 @@ class SuggestResult(object):
     Each result will come with the following:
 
     the variable json will contain the full json object of the result.
-    
+
     url: The URL for the suggestion. Internal to Bing, it seems
     query: The original query submited
     display_text: The suggestion generated
