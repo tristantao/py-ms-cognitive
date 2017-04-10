661c3bd235d6418f5f87cc0f2967d7b07f5e9b7b
tristantao <tao.tristan@gmail.com>
2017-04-09 18:06:00 -0700

    new custom_param for web search

--- py_ms_cognitive/py_ms_cognitive_search/py_ms_cognitive_web_search.py
+++ py_ms_cognitive/py_ms_cognitive_search/py_ms_cognitive_web_search.py
@@ -13,10 +13,9 @@ class PyMsCognitiveWebSearch(PyMsCognitiveSearch):
 
     SEARCH_WEB_BASE = 'https://api.cognitive.microsoft.com/bing/v5.0/search'
 
-    def __init__(self, api_key, query, silent_fail=False, custom_params=''):
+    def __init__(self, api_key, query, custom_params={}, silent_fail=False):
         query_url = self.SEARCH_WEB_BASE
-        self.custom_params_hash = dict(item.split("=") for item in custom_params.split("&")[1:])
-        PyMsCognitiveSearch.__init__(self, api_key, query, query_url, silent_fail=silent_fail)
+        PyMsCognitiveSearch.__init__(self, api_key, query, query_url, custom_params, silent_fail=silent_fail)
 
     def _search(self, limit, format):
         '''
@@ -28,7 +27,8 @@ class PyMsCognitiveWebSearch(PyMsCognitiveSearch):
           'count' : limit, #currently 50 is max per search.
           'offset': self.current_offset,
         }
-        payload.update(self.custom_params_hash)
+        payload.update(self.CUSTOM_PARAMS)
+
         headers = { 'Ocp-Apim-Subscription-Key' : self.api_key }
         if not self.silent_fail:
             QueryChecker.check_web_params(payload, headers)
